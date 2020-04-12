81c7e631c92b06d4425f8dde437426650bbc02ca
George Qi <george.qi@serko.com>
2020-04-13 11:27:02 +1200

    refactor

--- Algorithm/LastStoneWeight/Solution.cs
+++ Algorithm/LastStoneWeight/Solution.cs
@@ -1,64 +1,78 @@
 using System.Linq;
 using System.Collections.Generic;
 
-public class Solution {
-    public int LastStoneWeight(int[] stones) {
-        if(stones==null || stones.Length==0) return 0;
-        
-        var list = stones.OrderBy(s=>s).ToList();
-        var list2= new List<int>(stones.Length);
-        
-        var l = list.Count();
-        var k = 0;
-        while((l+k)>1){
-            var x = 0;
-            var y = 0;
-            
-            var x0 = k>0?list2[k-1]:-1;
-            var y0 = l>0?list[l-1]:-1;
-            if(x0>=y0){
-                k--;
-                list2.RemoveAt(k);
-                x = x0;
-                if(k==0){
-                    y = y0;
+public class Solution
+{
+    public int LastStoneWeight(int[] stones)
+    {
+        if (stones == null || stones.Length == 0) return 0;
+
+        var sortedStones = stones.OrderBy(s => s).ToList();
+        var debris = new List<int>(stones.Length);
+
+        var l = sortedStones.Count();
+        var debrisCount = 0;
+        while ((l + debrisCount) > 1)
+        {
+            var stone1 = 0;
+            var stone2 = 0;
+
+            var x0 = debrisCount > 0 ? debris[debrisCount - 1] : -1;
+            var y0 = l > 0 ? sortedStones[l - 1] : -1;
+            if (x0 >= y0)
+            {
+                debris.RemoveAt(--debrisCount);
+                stone1 = x0;
+                if (debrisCount == 0)
+                {
+                    stone2 = y0;
                     l--;
-                }else{
-                    x0 = list2[k-1];
-                    if(x0>=y0){
-                        k--;
-                        list2.RemoveAt(k);
-                        y= x0;
-                    }else{
+                }
+                else
+                {
+                    x0 = debris[debrisCount - 1];
+                    if (x0 >= y0)
+                    {
+                        debris.RemoveAt(--debrisCount);
+                        stone2 = x0;
+                    }
+                    else
+                    {
                         l--;
-                        y = y0;
+                        stone2 = y0;
                     }
                 }
-            }else{
+            }
+            else
+            {
                 l--;
-                x = y0;
-                
-                y0 = l>0?list[l-1]:-1;
-                if(x0>=y0){
-                    k--;
-                    list2.RemoveAt(k);
-                    y = x0;
-                }else{
+                stone1 = y0;
+
+                y0 = l > 0 ? sortedStones[l - 1] : -1;
+                if (x0 >= y0)
+                {
+                    debrisCount--;
+                    debris.RemoveAt(debrisCount);
+                    stone2 = x0;
+                }
+                else
+                {
                     l--;
-                    y = y0;
+                    stone2 = y0;
                 }
             }
-            
-            var w = x-y;
 
-            if(w>0){
-                var pos = list2.BinarySearch(w);
-				if(pos<0) pos=~pos;
-                list2.Insert(pos, w);
-                k++;
+            var remain = stone1 - stone2;
+
+            if (remain > 0)
+            {
+                var pos = debris.BinarySearch(remain);
+                if (pos < 0) pos = ~pos;
+                debris.Insert(pos, remain);
+                debrisCount++;
             }
         }
-        
-        return l==0?( k==0?0:list2[0]):list[0];
+
+        return l == 0 ? (debrisCount == 0 ? 0 : debris[0]) : sortedStones[0];
     }
 }
