ea399c0c1c18f130c91e32a6a6407fa8f8218709
George Qi <george.qi@serko.com>
2020-04-14 08:14:31 +1200

    brute force

--- Algorithm/ContiguousArray/Solution.cs
+++ Algorithm/ContiguousArray/Solution.cs
@@ -1,10 +1,48 @@
+using System;
+using System.Linq;
+
 ///<Summary>
 ///Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
 ///</Summary>
 public class Solution
 {
-    public int FindMaxLength(int[] nums)
+    public int FindMaxLength(int[] numbers)
     {
-        return 1;
+        if (numbers.Length < 2) return 0;
+        var zero = numbers.Count(x => x == 0);
+        var one = numbers.Length - zero;
+        var max = Math.Min(zero, one);
+
+        var result = 0;
+
+        // var maxs = new int[nums.Length];
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            var c = new int[2];
+
+            var length = 0;
+            for (var j = i; j < numbers.Length; j++)
+            {
+                c[numbers[j]]++;
+
+                if (c[0] == c[1])
+                {
+                    length = (j - i) + 1;
+                    c[0] = c[1] = 0;
+                    continue;
+                }
+
+                //couldn't find more
+                if (c[0] > max || c[1] > max)
+                {
+                    break;
+                }
+
+            }
+            result = Math.Max(result, length);
+
+        }
+
+        return result;
     }
 }
