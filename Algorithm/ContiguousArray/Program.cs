3e7997bf14b87473e416e009211216ab278bb806
George Qi <george.qi@serko.com>
2020-04-13 20:56:40 +1200

    add Contiguous Array

---
+++ Algorithm/ContiguousArray/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ContiguousArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+            if (s.FindMaxLength(new[] { 0, 1, 0, 1, 1, 0, 1 }) != 6
+                || s.FindMaxLength(new[] { 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0 }) != 6)
+            {
+                throw new ApplicationException("Incorrect");
+            }
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
---
+++ Algorithm/ContiguousArray/Solution.cs
@@ -0,0 +1,10 @@
+///<Summary>
+///Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+///</Summary>
+public class Solution
+{
+    public int FindMaxLength(int[] nums)
+    {
+        return 1;
+    }
+}
