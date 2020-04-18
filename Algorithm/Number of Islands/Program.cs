c4ba62737a7cb2e2e983c18083c3e0cff76957f3
George Qi <george.qi@serko.com>
2020-04-18 18:40:37 +1200

    add NumberOfIslands

---
+++ Algorithm/Number of Islands/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NumberOfIslands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+	        var result = s.NumIslands(new []{3,7,8});
+	
+            Console.WriteLine($"{result}");
+        }
+    }
+}
---
+++ Algorithm/Number of Islands/Solution.cs
@@ -0,0 +1,13 @@
+using System.Linq;
+using System.Collections.Generic;
+
+public class Solution
+{
+    ///<summary>
+    ///Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+    ///</summary>
+    public int  NumIslands(char[][] grid) {
+    {
+        throw new NotImplementedException();
+    }
+}
