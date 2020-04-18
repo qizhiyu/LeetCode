e7e38b9852a9b6d4b77b6f69a1c0dcc9dc7e6cd2
George Qi <george.qi@serko.com>
2020-04-18 18:47:28 +1200

    setup test case

--- Algorithm/Number of Islands/Program.cs
+++ Algorithm/Number of Islands/Program.cs
@@ -7,9 +7,19 @@ namespace NumberOfIslands
         static void Main(string[] args)
         {
             var s = new Solution();
-	        var result = s.NumIslands(new []{3,7,8});
-	
+            var result = s.NumIslands(new char[][]{
+                new[]{'1','0','1','0','0'},
+                new[]{'1','1','1','0','0'},
+                new[]{'0','0','0','1','1'},
+                new[]{'1','1','1','0','0'},
+                new[]{'1','0','1','0','0'},
+                });
+
             Console.WriteLine($"{result}");
+            if (result != 3)
+            {
+                throw new Exception("incorrect");
+            }
         }
     }
 }
--- Algorithm/Number of Islands/Solution.cs
+++ Algorithm/Number of Islands/Solution.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Collections.Generic;
 
@@ -6,7 +7,7 @@ public class Solution
     ///<summary>
     ///Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
     ///</summary>
-    public int  NumIslands(char[][] grid) {
+    public int NumIslands(char[][] grid)
     {
         throw new NotImplementedException();
     }
