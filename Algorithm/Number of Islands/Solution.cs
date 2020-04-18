889b507fffe77428ef3bf5ba84bc53bc9c4b54ae
George Qi <george.qi@serko.com>
2020-04-18 19:35:55 +1200

    first version

--- Algorithm/Number of Islands/Solution.cs
+++ Algorithm/Number of Islands/Solution.cs
@@ -9,6 +9,71 @@ public class Solution
     ///</summary>
     public int NumIslands(char[][] grid)
     {
-        throw new NotImplementedException();
+        var width = grid.Length;
+        if (width == 0) return 0;
+
+        var height = grid[0].Length;
+        var map = grid.Select(x => x.Select(y => 0).ToList()).ToList();
+        // var links = new Dictionary<int, int>();
+
+        var id = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                // water
+                if (grid[i][j] == '0') continue;
+
+                // touched
+                if (map[i][j] != 0)
+                    continue;
+
+                map[i][j] = ++id;
+
+                //update all neighbors 
+                Mark(map, grid, i, j, id);
+
+            }
+        }
+
+        return id - 1;
     }
+
+    void Mark(List<List<int>> map, char[][] grid, int i, int j, int id)
+    {
+        if (grid[i][j] == '1' && map[i][j] == 0)
+        {
+            map[i][j] = id;
+            MarkAllNeighbors(map, grid, i, j, id);
+        }
+    }
+
+    void MarkAllNeighbors(List<List<int>> map, char[][] grid, int i, int j, int id)
+    {
+        //left
+        if (i > 0)
+        {
+            Mark(map, grid, i - 1, j, id);
+        }
+
+        //up
+        if (j > 0)
+        {
+            Mark(map, grid, i, j - 1, id);
+        }
+
+        //right
+        if (j + 1 < grid[i].Length)
+        {
+            Mark(map, grid, i, j + 1, id);
+        }
+
+        //below
+        if (i + 1 < grid.Length)
+        {
+            Mark(map, grid, i + 1, j, id);
+        }
+
+    }
+
 }
